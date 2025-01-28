39452bab847e26af1da36c43ccea98e6c010bb8c
Riham Bassim <riham@Rihams-Laptop.local>
2025-01-28 17:41:41 +0100

    Model:Company

--- src/infrastructure/database/sequelize/associations/contract.associations.ts
+++ src/infrastructure/database/sequelize/associations/contract.associations.ts
@@ -1,5 +1,6 @@
 import { ContractModel } from "../models/contract.model";
 import { CompanyContractModel } from "../models/companyContract.model";
+import { CompanyModel } from "../models/company.model";
 
 CompanyContractModel.belongsTo(ContractModel, {
   foreignKey: "id_contract",
@@ -10,7 +11,16 @@ ContractModel.hasMany(CompanyContractModel, {
   as: "companyContract",
 });
 
-//TODO belongsTo relation Company-CompanyContract
-//TODO HasMany relation CompanyContract-Company
+//TODO relation ...
 
-export { CompanyContractModel, ContractModel };
+CompanyContractModel.belongsTo(CompanyModel, {
+  foreignKey: "id_company",
+  as: "company",
+});
+
+CompanyModel.hasMany(CompanyContractModel, {
+  foreignKey: "id_company",
+  as: "companyContract",
+});
+
+export { CompanyContractModel, ContractModel, CompanyModel };
