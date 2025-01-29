c999e96d04594c19f0073cfec875fe08fb1fa431
yu-Celik <celik.yus@outlook.com>
2025-01-29 10:29:35 +0100

    [add materialCm model]

---
+++ src/core/entities/materialCm.entity.ts
@@ -0,0 +1,10 @@
+export class MaterialCm {
+    constructor(
+        public id: number,
+        public id_c_employee: number,
+        public id_material: number,
+        public createdAt: Date,
+        public updatedAt: Date,
+        public isdeleted: boolean
+    ) { }
+}
