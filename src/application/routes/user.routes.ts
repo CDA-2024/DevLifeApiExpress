3b760708ab440060e4e61bc8b22849cbf49ed30a
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2025-01-30 10:50:43 +0100

    [feat] Refactor controllers and routes for materials and users; add materialSkill functionality

--- src/application/routes/user.routes.ts
+++ src/application/routes/user.routes.ts
@@ -1,8 +1,18 @@
 import { Router } from "express";
-import { getUsersController } from "../controllers/user.controller";
+import {
+  createUserController,
+  deleteUserController,
+  getUserByIdController,
+  getUsersController,
+  updateUserController,
+} from "../controllers/user.controller";
 
 const router = Router();
 
-router.get("/", getUsersController);
+router.get("/users", getUsersController);
+router.get("/users/:id", getUserByIdController);
+router.post("/users", createUserController);
+router.put("/users/:id", updateUserController);
+router.delete("/users/:id", deleteUserController);
 
 export default router;
