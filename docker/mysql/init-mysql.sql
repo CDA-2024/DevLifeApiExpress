752601a320931feb8156e403a2005128ccb05471
Riham Bassim <riham@Rihams-Laptop.local>
2025-01-30 11:19:54 +0100

    contractusecase

--- docker/mysql/init-mysql.sql
+++
@@ -1,3 +0,0 @@
-CREATE DATABASE IF NOT EXISTS `devlife_mysql`;
-GRANT ALL PRIVILEGES ON `devlife_mysql`.* TO 'devlife'@'%';
-FLUSH PRIVILEGES;
--- src/application/routes/index.ts
+++ src/application/routes/index.ts
@@ -1,10 +1,14 @@
 import { Router } from "express";
 import userRoutes from "./user.routes";
 import materialRoutes from "./material.routes";
+import contractRoutes from "./contract.routes";
+import companyContractRoutes from "./companyContract.routes";
 
 const router = Router();
 
 router.use("/users", userRoutes);
 router.use("/materials", materialRoutes);
+router.use("/contracts", contractRoutes);
+router.use("/companyContracts", companyContractRoutes);
 
 export default router;
