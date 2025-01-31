9a6b3780fbd7aa4ffd750d43c826118c1c8bf320
Antoine Caps <147519666+Aetherum97@users.noreply.github.com>
2025-01-31 14:07:04 +0100

    [edit] add charset utf8mb4 into mydsl db

--- src/infrastructure/database/sequelize/config/db.config.ts
+++ src/infrastructure/database/sequelize/config/db.config.ts
@@ -1,17 +1,23 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-
 dotenv.config();
 
 export const sequelize = new Sequelize(
-  process.env.MYSQL_DATABASE?? "",
-  process.env.MYSQL_USER?? "",
-  process.env.MYSQL_PASSWORD?? "",
+  process.env.MYSQL_DATABASE ?? "",
+  process.env.MYSQL_USER ?? "",
+  process.env.MYSQL_PASSWORD ?? "",
   {
-    host: process.env.MYSQL_HOST?? "",
+    host: process.env.MYSQL_HOST ?? "",
     port: Number(process.env.MYSQL_PORT),
     dialect: "mysql",
+    dialectOptions: {
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci",
+    },
+    define: {
+      charset: "utf8mb4",
+      collate: "utf8mb4_general_ci",
+    },
   }
 );
-
