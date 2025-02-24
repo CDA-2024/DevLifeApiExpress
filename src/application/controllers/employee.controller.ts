d8a488e89a370a7b9a810e6e46e2a07dc80d1885
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-02-24 20:13:38 +0100

    [fix]  employee controller fix

--- src/application/controllers/employee.controller.ts
+++ src/application/controllers/employee.controller.ts
@@ -3,10 +3,7 @@ import { EmployeeUseCase } from "../../core/use-cases/employee.use-case";
 
 const EmployeeCompagnyUseCase = new EmployeeUseCase();
 
-export const getEmployeesController = async (
-  req: Request,
-  res: Response
-) => {
+export const getEmployeesController = async (req: Request, res: Response) => {
   const materials = await EmployeeCompagnyUseCase.getAll();
   res.json(materials);
 };
@@ -19,18 +16,29 @@ export const getEmployeeByIdController = async (
   res.json(material);
 };
 
-export const saveEmployeeController = async (
-  req: Request,
-  res: Response
-) => {
-  const material = await EmployeeCompagnyUseCase.save(req.body);
-  res.json(material);
+export const saveEmployeeController = async (req: Request, res: Response) => {
+  try {
+    const employeeData = req.body;
+
+    const employee = await EmployeeCompagnyUseCase.save(employeeData);
+    res.status(201).json(employee);
+  } catch (error) {
+    console.error("Error saving employee:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while saving the employee" });
+  }
 };
 
-export const deleteEmployeeController = async (
-  req: Request,
-  res: Response
-) => {
-  await EmployeeCompagnyUseCase.delete(req.params.id);
-  res.json();
+export const deleteEmployeeController = async (req: Request, res: Response) => {
+  try {
+    const employeeId = req.params.id;
+    await EmployeeCompagnyUseCase.delete(employeeId);
+    res.status(200).json({ message: "Employee deleted successfully" });
+  } catch (error) {
+    console.error("Error deleting employee:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while deleting the employee" });
+  }
 };
