eb7b4ccf9275e00f8466bbd20117fafb0613d423
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-02-24 20:12:35 +0100

    [feat] added cors

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,12 @@ const app = express();
 app.use(express.json());
 app.use("/api", routes);
 // TODO: implement this -> app.use(errorHandler);
-// TODO: add CORS
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 // parser les cookies
 app.use(cookieParser());
